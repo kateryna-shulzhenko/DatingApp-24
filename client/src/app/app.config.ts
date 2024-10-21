39ea772f03b33bf2e179d7f27a7ad09be27c95a4
kateryna-shulzhenko <kateryna.shulzhenko@visma.com>
2024-10-21 13:31:06 +0200

    section-6

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,14 +1,22 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, NgModule} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import {provideAnimations} from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideToastr } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
-    provideAnimations()
+    provideAnimations(),
+    provideToastr({
+      positionClass: 'toast-bottom-right'
+    })
   ]
 };
+function provideToastr(arg0: { positionClass: string; }): import("@angular/core").Provider | import("@angular/core").EnvironmentProviders {
+  throw new Error('Function not implemented.');
+}
+
